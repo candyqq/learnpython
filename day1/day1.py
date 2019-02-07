d8aee0f0f16e8e4be82e6f3742ce0015d21ca125
candyqq <1526582202@qq.com>
2019-02-07 21:40:10 +0800

    c

---
+++ day1/day1.py
@@ -0,0 +1,27 @@
+#encoding:utf-8
+
+print("""
+line1 
+line2 
+line3
+""")
+
+print("line1"
+      "line2"
+      "line3")
+print("I'm OK \t OK")
+
+#使用list 和tuple
+classemate=['Micheal','Bob','Tracy']
+print(classemate)
+print(classemate[0])
+print(len(classemate))
+
+#条件判断
+age = 3
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+else:
+    print('your age is', age)
+    print('teenager')
